30787064c3fed32ccad93422017b7514f5934140
unknown <n02531357@LC112PC02.active.newpaltz.edu>
2014-09-08 09:44:44 -0400

    Create index.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>Home Page</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by n02531357
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
