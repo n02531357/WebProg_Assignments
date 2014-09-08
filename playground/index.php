3d1d86e25730da9189dccb4355b6e0852743f32a
unknown <n02531357@LC112PC02.active.newpaltz.edu>
2014-09-08 10:40:26 -0400

    Add CDN

--- playground/index.php
+++ playground/index.php
@@ -32,5 +32,7 @@
 				</p>
 			</footer>
 		</div>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
