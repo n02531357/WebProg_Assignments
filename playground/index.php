d75f309e2a2fe2fa8560471ca1ec928303d8ba9e
unknown <n02531357@LC112PC02.active.newpaltz.edu>
2014-09-08 10:38:29 -0400

    ...

--- playground/index.php
+++ playground/index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 	</head>
 
 	<body>
