dd9d70747c2bb55450379c5167efd708e63acbf6
Charles Palsa <n02531357@hawkmail.newpaltz.edu>
2014-09-11 04:34:51 +0000

    Basic forms and layout added.

--- playground/index.php
+++ playground/index.php
@@ -3,28 +3,90 @@
 	<head>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" type="text/css" href=trackerCSS.css>
 	</head>
 
 	<body>
 		<div class="container">
 			<header>
-				<h1>Home Page</h1>
+				<h1>Nutritional Profile</h1>
 			</header>
 		</div>
+		
 		<div class="container">
-			<nav>
-				<p>
-					<a href="/">Home</a>
-				</p>
-				<p>
-					<a href="/contact">Contact</a>
-				</p>
+			<nav class="nav-bar">
+				<ul class="link-list">
+					<li><a href="/">Profile</a></li>
+					<li><a href="mailto:n02531357@hawkmail.newpaltz.edu?Subject=Fitness%20Tracker">Contact</a></li>
+				</ul>
 			</nav>
 		</div>
-		<div class='container'><p>Sample Content</p></div>			
+		
+		<div class="container">	
+			<div id="form">
+				<div class='container'>
+					<div class="food">
+						<p id="breakfast">Breakfast</p>
+						<form>
+							Food: <input type="text" name="food">
+							Calories: <input type="text" name="calories">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class='container'>
+					<div class="food">
+						<p id="lunch">Lunch</p>
+						<form>
+							Food: <input type="text" name="food">
+							Calories: <input type="text" name="calories">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class='container'>
+					<div class="food">
+						<p id="dinner">Dinner</p>
+						<form>
+							Food: <input type="text" name="food">
+							Calories: <input type="text" name="calories">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class='container'>
+					<div class="food">
+						<p id="snack">Snacks</p>
+						<form>
+							Food: <input type="text" name="food">
+							Calories: <input type="text" name="calories">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class='container'>
+					<div class="workout">
+						<p id="excercise">Excercise</p>
+						<form>
+							Exercise: <input type="text" name="workout">
+							Calories: <input type="text" name="calories_burned">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container">
+			<p>Table displaying entered values and totals will go here<p>		
+		</div>		
+		
 		<div class="container">
 			<footer>
 				<p>
