2903fff300b73d1957ed9374a844f73c1b259e79
unknown <n02531357@LC112PC02.active.newpaltz.edu>
2014-09-15 10:23:35 -0400

    formatting

--- playground/profile.php
+++ playground/profile.php
@@ -177,7 +177,6 @@
 	</div>
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js">
-</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
