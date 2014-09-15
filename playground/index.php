a175689b3765d550b155cb1307ac02159cd52619
Charles Palsa <charles@Caffeine.gs>
2014-09-15 08:52:58 -0400

    Utilized boot strap features

--- playground/index.php
+++ playground/index.php
@@ -1,63 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-	</head>
-	
-	<style>
-	</style>
 
-	<body>
-	<div class="container">
-		<div class="page-header">
-			<h1>Nutritional Home</h1>
-		</div>
-	</div>
-	
-	<div class="container">	
-		<ul class="nav nav-pills">
-	  		<li class="active"><a href="index.php">Home</a></li>
-	  		<li><a href="profile.php">Profile</a></li>
-	 		<li><a href="#">Contact</a></li>
-		</ul>
-	</div>
-	
-		<div class="container">
-			<p>General news and things. Sign up/in, Welcome page, etc.<p>
-				<div class="container">		
-					<form class="form-inline" role="form">
-						 <div class="form-group">
-						    <div class="input-group">
-						      <div class="input-group-addon">@</div>
-						      <input class="form-control" type="email" placeholder="Enter email">
-						    </div>
-						  </div>
-						  <div class="form-group">
-						    <label class="sr-only" for="exampleInputPassword2">Password</label>
-						    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-						  </div>
-						  <div class="checkbox">
-						    <label>
-						      <input type="checkbox"> Remember me
-						    </label>
-						  </div>
-						  <button type="submit" class="btn btn-default">Sign in</button>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+</head>
+
+<style type="text/css">
+    .page-header {
+        position: relative;
+        padding: 30px 15px;
+        color: #FFFFFF;
+        text-align: center;
+        text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+        background-image: linear-gradient(to bottom, #006600 0px, #009933 100%);
+        background-repeat: repeat-x;
+    }
+    
+	.page-header p {
+		color: #A6CAA6;
+	  	text-align: center;
+	  	text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+	}    
+
+	a, a:hover {
+	 	color: #006600;
+	}
+</style>
+<body>
+<div class="container">
+    <header>
+        <div class="container">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/~n02531357/WebProgramming/WebProg_Assignments/playground/">Home</a>
+                </li>
+                <li>
+                    <a href="/~n02531357/WebProgramming/WebProg_Assignments/playground/profile.php">Profile</a>
+                </li>
+                <li>
+                    <a href="#">Forum</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Welcome</h1>
+            <p>This web application's purpose is to assist the user in tracking their nutritional and fitness goals.</p>
+        </div>
+    </div>
+    
+    <div class="container" style="padding-bottom: 25px;">
+	    <div class="row">
+	    	<div class="col-md-2"></div>
+	  		<div class="col-md-3">
+	  			<h3><em>Sign in!</em></h3>
+	  				<form role="form">
+					  	<div class="form-group">
+						    <label for="signInEmail">Email address</label>
+						    <input type="email" class="form-control" id="signInEmail" placeholder="Enter email">
+						</div>
+						<div class="form-group">
+						    <label for="signInPw">Password</label>
+						    <input type="password" class="form-control" id="signInPw" placeholder="Password">
+						</div>
+						<button type="button" class="btn btn-success">Sign in</button>
 					</form>
-				</div>			
-		</div>		
-		
-		<div class="container">
-			<footer>
-				<p>
-					&copy; Copyright  by n02531357
-				</p>
-			</footer>		
+	  		</div>
+	  		<div class="col-md-2" style="text-align: center; margin-top: 50px;"><h2><em>or</em></h2></div>
+	  		<div class="col-md-3">
+	  			<h3><em>Sign up!</em></h3>
+	  				<form role="form">
+					  	<div class="form-group">
+						    <label for="signUpEmail">Email address</label>
+						    <input type="email" class="form-control" id="signUpEmail" placeholder="Enter email">
+						</div>
+						<div class="form-group">
+						    <label for="signUpPw">Password</label>
+						    <input type="password" class="form-control" id="signUpPw" placeholder="Password">
+						</div>
+						<div class="form-group">
+						    <label for="signUpPwCheck">Confirm Password</label>
+						    <input type="password" class="form-control" id="signUpPwCheck" placeholder="Password again">
+						</div>						
+						<div class="form-group">
+						    <label for="signUpFName">First Name</label>
+						    <input type="name" class="form-control" id="signUpFName" placeholder="Ex. John">
+						</div>
+						<div class="form-group">
+						    <label for="signUpLName">Last Name</label>
+						    <input type="name" class="form-control" id="signUpLName" placeholder="Ex. Smith">
+						</div>						
+						<button type="button" class="btn btn-success">Sign up</button>
+					</form>	  			
+	  		</div>
+	  		<div class="col-md-2"></div>
 		</div>
+    </div>
+</div>  
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</body>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+
 </html>
--- playground/profile.php
+++ playground/profile.php
@@ -1,104 +1,171 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-	</head>
-	
-	<style>
-	    .form-horizontal {
-	        padding-top:25px;
-	        padding-bottom:25px;
-	    }
-	</style>
 
-	<body>
-	<div class="container">
-		<div class="page-header">
-			<h1>Nutritional Profile</h1>
-		</div>
-	</div>
-	
-	<div class="container">	
-		<ul class="nav nav-pills">
-	  		<li><a href="index.php">Home</a></li>
-	  		<li class="active"><a href="profile.php">Profile</a></li>
-	 		<li><a href="#">Contact</a></li>
-		</ul>
-	</div>	
-	
-	<div class="container">
-        <form class="form-horizontal" role="form">
-              <div class="form-group">
-                <label for="inputFood" class="col-sm-2 control-label">Food</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputFood" placeholder="What you ate">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputCalories" class="col-sm-2 control-label">Calories</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputCalories" placeholder="Calories consumed">
-                </div>
-              </div>
-               <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    <select>
-                      <option>Breakfast</option>
-                      <option>Lunch</option>
-                      <option>Dinner</option>
-                      <option>Snack</option>
-                    </select>
-                    </div>
-                </div>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+</head>
+
+<style type="text/css">
+    .page-header {
+        position: relative;
+        padding: 30px 15px;
+        color: #FFFFFF;
+        text-align: center;
+        text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+        background-image: linear-gradient(to bottom, #006600 0px, #009933 100%);
+        background-repeat: repeat-x;
+    }
     
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-default">Add to food diary</button>
-                </div>
-              </div>
-        </form>	
-	</div>
-	
-	<div class="container">
-        <form class="form-horizontal" role="form">
-              <div class="form-group">
-                <label for="inputWorkout" class="col-sm-2 control-label">Food</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputWorkout" placeholder="Excercise title">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputCalories" class="col-sm-2 control-label">Calories</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputCalories" placeholder="Calories burned">
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-default">Add to excercise diary</button>
-                </div>
-              </div>
-        </form>	
-	</div>
-	
-	<div class="container">
-		<p>
-			Table to be filled with data.
-		</p>
-	</div>
-	
-		<div class="container">
-			<footer>
-				<p>
-					&copy; Copyright  by n02531357
-				</p>
-			</footer>		
-		</div>
+		.page-header p {
+				color: #A6CAA6;
+	  	  text-align: center;
+	  	  text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+		}    
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</body>
+		a, a:hover {
+	 			color: #006600;
+		}
+</style>
+
+<body>
+<div class="container">
+    <header>
+        <div class="container">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/~n02531357/WebProgramming/WebProg_Assignments/playground/">Home</a>
+                </li>
+                <li>
+                    <a href="/~n02531357/WebProgramming/WebProg_Assignments/playground/profile.php">Profile</a>
+                </li>
+                <li>
+                    <a href="#">Forum</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Nutritional Profile</h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="active"><a href="#summary" role="tab" data-toggle="tab">Progress <span class="glyphicon glyphicon-road"></a>
+            </li>
+            <li><a href="#food" role="tab" data-toggle="tab">Food <span class="glyphicon glyphicon-cutlery"></span></a>
+            </li>
+            <li><a href="#excercise" role="tab" data-toggle="tab">Excercise <span class="glyphicon glyphicon-fire"></a>
+            </li>
+            <li><a href="#messages" role="tab" data-toggle="tab">Goals <span class="glyphicon glyphicon-star"></a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane fade in active" id="summary">
+	            <div class="row">
+	            	<div class="col-md-6">
+										<div class="panel panel-default">
+										  <div class="panel-heading">
+										    <h3 class="panel-title">Today's Calorie Goal:</h3>
+										  </div>
+										  <div class="panel-body">
+										    <div class="progress">
+		                    	<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 60%;" data-toggle="tooltip" data-placement="left" title="You have consumed 1000 of your 2000 calorie goal."></div>
+		                		</div>
+										  </div>
+										</div>
+		            </div>
+	            	<div class="col-md-6">
+										<div class="panel panel-default">
+										  <div class="panel-heading">
+										    <h3 class="panel-title">Today's Excercise Goal:</h3>
+										  </div>
+										  <div class="panel-body">
+										    <div class="progress">
+		                    	<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 60%;" data-toggle="tooltip" data-placement="right" title="You have burned 1000 of your 2000 calorie goal."></div>
+		                		</div>
+										  </div>
+										</div>
+		            </div>		            
+		          </div>
+            </div>
+            	<div class="row">
+            	   <div class="col-md-6">
+										<div class="panel panel-default">
+										  <div class="panel-heading">
+										    <h3 class="panel-title">Today's Meal Graph:</h3>
+										  </div>
+										  <div class="panel-body">
+										  </div>
+										</div>
+		            </div>
+	            	<div class="col-md-6">
+										<div class="panel panel-default">
+										  <div class="panel-heading">
+										    <h3 class="panel-title">Today's Excercise Graph:</h3>
+										  </div>
+										  <div class="panel-body">
+										  </div>
+										</div>
+		            </div>		            
+  						</div>
+            <div class="tab-pane fade" id="food">
+		          <div class="container">
+		            <div class ="row">
+									<form class="form-inline" role="form">
+									  <div class="form-group">
+									    <label class="sr-only" for="Time">Time</label>
+									    <div class="col-md-2">
+									    	<input type="text" class="form-control" id="timeOfMeal" placeholder="Time of Meal (Ex. 12:33)"></div>
+									    </div>
+									    <div class="col-md-1"></div>
+									    <div class="col-md-2">
+									    	<input type="text" class="form-control" id="foodTitle" placeholder="Name of food"></div>
+									    </div>
+									    <div class="col-md-1"></div>
+									    <div class="col-md-2">
+									    	<input type="text" class="form-control" id="servings" placeholder="# of Servings"></div>
+									    </div>
+									    <div class="col-md-1"></div>
+									    <div class="col-md-2">
+									    	<input type="text" class="form-control" id="caloriesps" placeholder="Calories per serving"></div>
+									    </div>
+									    <div class="col-md-1"><button type="submit" class="btn btn-success">Add <span class="glyphicon glyphicon-pencil"></button></div>
+		            </div>
+		          </div>
+		          <div class="container">
+							    <div class="table-responsive">
+										<table class="table table-hover">
+											<thead>
+												<tr>
+													<th>Time</th>
+													<th>Food</th>
+													<th>Servings</th>
+													<th>Calories per Serving<th>
+													<th>Total Calories<th>
+												</tr>
+											</thead>
+										</table>
+								</div>
+							</div>
+		          </div>
+            <div class="tab-pane fade" id="excercise"><p>Excercise</p></div>
+            <div class="tab-pane fade" id="messages"></div>
+				</div>
+			</div>
+</div>
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+
 </html>
